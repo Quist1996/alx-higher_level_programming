1c121e2ec828ad6726cd5f6afc8aebb45dc62301
Daniel <danielquist224@gmail.com>
2024-01-22 04:13:46 -0800

    done

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,40 +1,33 @@
 #include "lists.h"
+
 /**
-* insert_node - Inserts a number into a sorted singly-linked list.
-* @head: A pointer the head of the linked list.
-* @number: The number to insert.
-* Return: 0 If the function fails or pointer to the new node.
-*/
+ * insert_node - Inserts a number into a sorted singly-linked list.
+ * @head: A pointer the head of the linked list.
+ * @number: The number to insert.
+ * Return: 0 If the function fails or pointer to the new node.
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-listint_t *node = *head, *new;
-new = malloc(sizeof(listint_t));
-if (new == NULL)
-return (NULL);
-new->n = number;
-if (node == NULL || node->n >= number)
-{
-new->next = node;
-*head = new;
-return (new);
+	listint_t *node = *head, *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
 }
-while (node && node->next && node->next->n < number)
-node = node->next;
-new->next = node->next;
-node->next = new;
-return (new);
-}
-100-print_tehbapla.py
-#!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-if i % 2 == 0:
-diff = 0
-else:
-diff = 32
-print('{}'.format(chr(i - diff)), end='')
-101-revove_char_at.py
-#!/usr/bin/python3
-def remove_char_at(str, n):
-if n < 0:
-return (str)
-return (str[:n] + str[n+1:])
+
